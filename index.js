4f97c6b0bb58841b6f7a8de668d12aaf226ef12a
Roman Stigler <fromanx@gmail.com>
2022-11-14 16:18:34 -0600

    added functionality to create readme file in js

--- index.js
+++ index.js
@@ -2,13 +2,85 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = ["What is your github username?", "What is your email address?", "What is your project's name?", "Please write a short description of your project:",
+"What kind of license should your project have?", "What command should be run to install dependencies?", "What command should be run to run tests?",
+ "What does the user need to know about using the repo?", "What does the user need to know about contributing to the repo?"];
 
 // TODO: Create a function to write README file
-function writeToFile(README.md, data) {}
+const generateREADME = ({userName, email, projectName, description, license, installCommand, testCommand, userInfo, contributeInfo}) =>
+`#Title
 
+${projectName} by ${userName}. Email is ${email}.
+
+##Description
+
+${description}
+
+##License
+
+${license}
+${installCommand}
+${testCommand}
+${userInfo}
+${contributeInfo}
+`;
+
+function init() { inquirer.prompt([
+    {
+        type: 'input',
+        name: 'userName',
+        message: questions[0],
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: questions[1],
+    },
+    {
+        type: 'input',
+        name: 'projectName',
+        message: questions[2],
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: questions[3],
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: questions[4],
+        choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None']
+    },
+    {
+        type: 'input',
+        name: 'installCommand',
+        message: questions[5],
+    },
+    {
+        type: 'input',
+        name: 'testCommand',
+        message: questions[6],
+    },
+    {
+        type: 'input',
+        name: 'userInfo',
+        message: questions[7],
+    },
+    {
+        type: 'input',
+        name: 'contributeInfo',
+        message: questions[8],
+    }
+])
 // TODO: Create a function to initialize app
-function init() {}
+.then((answers) => {
+        const readmeContent = generateREADME(answers);
 
+        fs.writeFile("README.md", readmeContent, (error) => 
+            error ? console.error(error) : console.log("README successfully generated!")
+        );
+        });
+}
 // Function call to initialize app
 init();
