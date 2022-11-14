c37bb60f479e7d3f4f5320d03a014f7f462f5f9a
Roman Stigler <fromanx@gmail.com>
2022-11-14 10:29:40 -0600

    added code for necessary packages

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require("inquirer");
+const fs = require("fs");
 // TODO: Create an array of questions for user input
 const questions = [];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(README.md, data) {}
 
 // TODO: Create a function to initialize app
 function init() {}
